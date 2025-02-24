0b63e99a2c766aa490dfe943b53d607dffbea9b9
Joilton Silva <devbackgruposna@gmail.com>
2025-02-24 09:25:44 -0300

    chore(project): adjust build. (👤: joilton silva)

--- manage_translations.py
+++ manage_translations.py
@@ -7,88 +7,92 @@ import inquirer
 
 from task_commit.utils import color_text, get_translator
 
-LOCALES_DIR = "locales"
-POT_FILE = os.path.join(LOCALES_DIR, "messages.pot")
-PO_FILE = os.path.join(LOCALES_DIR, "pt_BR/LC_MESSAGES/messages.po")
-MO_FILE = os.path.join(LOCALES_DIR, "pt_BR/LC_MESSAGES/messages.mo")
-SOURCE_DIR = "task_commit"
+LOCALES_DIR = locale_dir = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    'task_commit',
+    'locales',
+)
+POT_FILE = os.path.join(LOCALES_DIR, 'messages.pot')
+PO_FILE = os.path.join(LOCALES_DIR, 'pt_BR/LC_MESSAGES/messages.po')
+MO_FILE = os.path.join(LOCALES_DIR, 'pt_BR/LC_MESSAGES/messages.mo')
+SOURCE_DIR = 'task_commit'
 
 LANGUAGES = {
-    "af": "Afrikaans",
-    "ar": "Arabic",
-    "az": "Azerbaijani",
-    "be": "Belarusian",
-    "bg": "Bulgarian",
-    "bn": "Bengali",
-    "bs": "Bosnian",
-    "ca": "Catalan",
-    "cs": "Czech",
-    "cy": "Welsh",
-    "da": "Danish",
-    "de": "German",
-    "el": "Greek",
-    "en": "English",
-    "eo": "Esperanto",
-    "es": "Spanish",
-    "et": "Estonian",
-    "eu": "Basque",
-    "fa": "Persian",
-    "fi": "Finnish",
-    "fo": "Faroese",
-    "fr": "French",
-    "ga": "Irish",
-    "gl": "Galician",
-    "gu": "Gujarati",
-    "he": "Hebrew",
-    "hi": "Hindi",
-    "hr": "Croatian",
-    "hu": "Hungarian",
-    "hy": "Armenian",
-    "id": "Indonesian",
-    "is": "Icelandic",
-    "it": "Italian",
-    "ja": "Japanese",
-    "ka": "Georgian",
-    "kk": "Kazakh",
-    "km": "Khmer",
-    "kn": "Kanari",
-    "ko": "Korean",
-    "lt": "Lithuanian",
-    "lv": "Latvian",
-    "mk": "Macedonian",
-    "ml": "Malayalam",
-    "mn": "Mongolian",
-    "mr": "Marathi",
-    "ms": "Malay",
-    "mt": "Maltese",
-    "nb": "Norwegian Bokmål",
-    "ne": "Nepali",
-    "nl": "Dutch",
-    "nn": "Norwegian Nynorsk",
-    "pa": "Punjabi",
-    "pl": "Polish",
-    "pt": "Portuguese",
-    "pt_BR": "Portuguese (Brazil)",
-    "pt_PT": "Portuguese (Portugal)",
-    "ro": "Romanian",
-    "ru": "Russian",
-    "si": "Sinhala",
-    "sk": "Slovak",
-    "sl": "Slovene",
-    "sq": "Albanian",
-    "sr": "Serbian",
-    "sv": "Swedish",
-    "sw": "Swahili",
-    "ta": "Tamil",
-    "te": "Telugu",
-    "th": "Thai",
-    "tr": "Turkish",
-    "uk": "Ukrainian",
-    "ur": "Urdu",
-    "vi": "Vietnamese",
-    "zh_CN": "Simplified Chinese",
-    "zh_TW": "Traditional Chinese",
-    "zu": "Zulu",
+    'af': 'Afrikaans',
+    'ar': 'Arabic',
+    'az': 'Azerbaijani',
+    'be': 'Belarusian',
+    'bg': 'Bulgarian',
+    'bn': 'Bengali',
+    'bs': 'Bosnian',
+    'ca': 'Catalan',
+    'cs': 'Czech',
+    'cy': 'Welsh',
+    'da': 'Danish',
+    'de': 'German',
+    'el': 'Greek',
+    'en': 'English',
+    'eo': 'Esperanto',
+    'es': 'Spanish',
+    'et': 'Estonian',
+    'eu': 'Basque',
+    'fa': 'Persian',
+    'fi': 'Finnish',
+    'fo': 'Faroese',
+    'fr': 'French',
+    'ga': 'Irish',
+    'gl': 'Galician',
+    'gu': 'Gujarati',
+    'he': 'Hebrew',
+    'hi': 'Hindi',
+    'hr': 'Croatian',
+    'hu': 'Hungarian',
+    'hy': 'Armenian',
+    'id': 'Indonesian',
+    'is': 'Icelandic',
+    'it': 'Italian',
+    'ja': 'Japanese',
+    'ka': 'Georgian',
+    'kk': 'Kazakh',
+    'km': 'Khmer',
+    'kn': 'Kanari',
+    'ko': 'Korean',
+    'lt': 'Lithuanian',
+    'lv': 'Latvian',
+    'mk': 'Macedonian',
+    'ml': 'Malayalam',
+    'mn': 'Mongolian',
+    'mr': 'Marathi',
+    'ms': 'Malay',
+    'mt': 'Maltese',
+    'nb': 'Norwegian Bokmål',
+    'ne': 'Nepali',
+    'nl': 'Dutch',
+    'nn': 'Norwegian Nynorsk',
+    'pa': 'Punjabi',
+    'pl': 'Polish',
+    'pt': 'Portuguese',
+    'pt_BR': 'Portuguese (Brazil)',
+    'pt_PT': 'Portuguese (Portugal)',
+    'ro': 'Romanian',
+    'ru': 'Russian',
+    'si': 'Sinhala',
+    'sk': 'Slovak',
+    'sl': 'Slovene',
+    'sq': 'Albanian',
+    'sr': 'Serbian',
+    'sv': 'Swedish',
+    'sw': 'Swahili',
+    'ta': 'Tamil',
+    'te': 'Telugu',
+    'th': 'Thai',
+    'tr': 'Turkish',
+    'uk': 'Ukrainian',
+    'ur': 'Urdu',
+    'vi': 'Vietnamese',
+    'zh_CN': 'Simplified Chinese',
+    'zh_TW': 'Traditional Chinese',
+    'zu': 'Zulu',
 }
 
 _ = get_translator()
@@ -104,10 +108,10 @@ def find_file(file: str, path: str, list_return: bool = False) -> str | list:
 def find_python_files(directory) -> list:
     """Walks through the directory and returns a list of all .py files."""
     python_files = []
-    python_files.append(__file__.split("/")[-1])
+    python_files.append(__file__.split('/')[-1])
     for root, _, files in os.walk(directory):
         for file in files:
-            if file.endswith(".py"):
+            if file.endswith('.py'):
                 python_files.append(os.path.join(root, file))
     return python_files
 
@@ -122,36 +126,39 @@ def run_command(command) -> None:
         check=True,
     )
     if result.returncode != 0:
-        message: str = _("Error executing command")
-        print(color_text(f"❌ {message}: {command}\n{result.stderr}", "red"))
+        message: str = _('Error executing command')
+        print(color_text(f'❌ {message}: {command}\n{result.stderr}', 'red'))
         sys.exit(1)
 
 
 def input_language() -> str:
     """Asks the user to input a language code and returns it."""
     languages_choices = [
-        {"name": f"{name} ({code})", "value": code} for code, name in LANGUAGES.items()
+        {'name': f'{name} ({code})', 'value': code}
+        for code, name in LANGUAGES.items()
     ]
     try:
         questions = [
             inquirer.List(
-                "language_choices",
-                message=_("Choose a language"),
-                choices=[lang["name"] for lang in languages_choices],
+                'language_choices',
+                message=_('Choose a language'),
+                choices=[lang['name'] for lang in languages_choices],
                 carousel=True,
             )
         ]
         answers = inquirer.prompt(questions)
         return next(
-            lang["value"]
+            lang['value']
             for lang in languages_choices
-            if lang["name"] == answers["language_choices"]
+            if lang['name'] == answers['language_choices']
         )
     except (KeyboardInterrupt, TypeError):
-        print(color_text(f'🚩 {_("Process interrupted. Exiting...")}', "red"))
+        print(color_text(f'🚩 {_("Process interrupted. Exiting...")}', 'red'))
         sys.exit(0)
     except Exception as error:
-        print(color_text(f'❌ {_("Unexpected error occurred")}: {error}', "red"))
+        print(
+            color_text(f'❌ {_("Unexpected error occurred")}: {error}', 'red')
+        )
         sys.exit(1)
 
 
@@ -162,11 +169,11 @@ def update_translations() -> None:  # noqa: PLR0912, PLR0915
     args = args[1:] if len(args) > 1 else None
     if args:
         match args[0]:
-            case "addlanguage":
+            case 'addlanguage':
                 language = input_language()
 
-                message: str = _("Adding language")
-                print(color_text(f"🌍 {message}: {language}", "green"))
+                message: str = _('Adding language')
+                print(color_text(f'🌍 {message}: {language}', 'green'))
 
                 if not os.path.exists(LOCALES_DIR):
                     os.mkdir(LOCALES_DIR)
@@ -174,22 +181,22 @@ def update_translations() -> None:  # noqa: PLR0912, PLR0915
                 if not os.path.exists(os.path.join(LOCALES_DIR, language)):
                     os.mkdir(os.path.join(LOCALES_DIR, language))
                 else:
-                    message: str = _("Language already exists")
-                    print(color_text(f"⚠️ {message}", "yellow"))
+                    message: str = _('Language already exists')
+                    print(color_text(f'⚠️ {message}', 'yellow'))
                     return
 
                 if not os.path.exists(
                     os.path.join(
                         LOCALES_DIR,
                         language,
-                        "LC_MESSAGES",
+                        'LC_MESSAGES',
                     )
                 ):
                     os.mkdir(
                         os.path.join(
                             LOCALES_DIR,
                             language,
-                            "LC_MESSAGES",
+                            'LC_MESSAGES',
                         )
                     )
 
@@ -197,17 +204,17 @@ def update_translations() -> None:  # noqa: PLR0912, PLR0915
                     os.path.join(
                         LOCALES_DIR,
                         language,
-                        "LC_MESSAGES",
-                        "messages.po",
+                        'LC_MESSAGES',
+                        'messages.po',
                     )
                 ):
                     # Listing all Python files
                     source_files = find_python_files(SOURCE_DIR)
                     if not source_files:
                         message: str = _(
-                            "No Python files found for extraction",
+                            'No Python files found for extraction',
                         )
-                        print(color_text(f"⚠️ {message}", "red"))
+                        print(color_text(f'⚠️ {message}', 'red'))
                         return
                     # Creating the .pot file
                     run_command(
@@ -231,59 +238,59 @@ def update_translations() -> None:  # noqa: PLR0912, PLR0915
                     mo_file: str = os.path.join(
                         LOCALES_DIR,
                         language,
-                        "LC_MESSAGES",
-                        "messages.mo",
+                        'LC_MESSAGES',
+                        'messages.mo',
                     )
-                    run_command(f"touch {mo_file}")
+                    run_command(f'touch {mo_file}')
 
                     # Removing the .pot file
-                    run_command(f"rm {POT_FILE}")
+                    run_command(f'rm {POT_FILE}')
 
-                    message: str = _("Language added successfully!")
-                    print(color_text(f"✅ {message}", "green"))
+                    message: str = _('Language added successfully!')
+                    print(color_text(f'✅ {message}', 'green'))
 
-            case "update":
-                message: str = _("Updating translations...")
-                print(color_text(f"📥 {message}", "green"))
+            case 'update':
+                message: str = _('Updating translations...')
+                print(color_text(f'📥 {message}', 'green'))
                 # Listing all Python files
                 source_files = find_python_files(SOURCE_DIR)
 
-                message: str = _("Python files found:")
-                print(color_text(f"📂 {message}", "green"))
+                message: str = _('Python files found:')
+                print(color_text(f'📂 {message}', 'green'))
                 for file in source_files:
-                    print(color_text(f"  📄 {file}", "green"))
+                    print(color_text(f'  📄 {file}', 'green'))
 
                 if not source_files:
-                    message: str = _("No Python files found for extraction")
-                    print(color_text(f"⚠️ {message}", "red"))
+                    message: str = _('No Python files found for extraction')
+                    print(color_text(f'⚠️ {message}', 'red'))
                     return
 
                 # Extracting new strings into the .pot file
-                message: str = _("Extracting strings to .pot file...")
-                print(color_text(f"📥 {message}", "green"))
+                message: str = _('Extracting strings to .pot file...')
+                print(color_text(f'📥 {message}', 'green'))
                 run_command(f'xgettext -o {POT_FILE} {" ".join(source_files)}')
 
                 # Updating the .po file without losing previous translations
                 if os.path.exists(
                     find_file(
-                        "messages.po",
+                        'messages.po',
                         LOCALES_DIR,
                     )
                 ):
-                    message: str = _("Updating existing translations...")
-                    print(color_text(f"📦 {message}", "green"))
+                    message: str = _('Updating existing translations...')
+                    print(color_text(f'📦 {message}', 'green'))
                     po_files = find_file(
-                        "messages.po",
+                        'messages.po',
                         LOCALES_DIR,
                         list_return=True,
                     )
                     for po_file in po_files:
-                        run_command(f"msgmerge --update {po_file} {POT_FILE}")
+                        run_command(f'msgmerge --update {po_file} {POT_FILE}')
 
                 else:
-                    message: str = _("File not found:")
-                    message_: str = _("Creating a new")
-                    print(f"⚠️ {message} {PO_FILE}. {message_}")
+                    message: str = _('File not found:')
+                    message_: str = _('Creating a new')
+                    print(f'⚠️ {message} {PO_FILE}. {message_}')
                     os.system(
                         f'cp {POT_FILE} {
                             os.path.join(LOCALES_DIR, "messages.po")
@@ -291,56 +298,56 @@ def update_translations() -> None:  # noqa: PLR0912, PLR0915
                     )
 
                 # Compiling the .mo file
-                message: str = _("Compiling .mo file...")
-                print(color_text(f"📦 {message}", "green"))
+                message: str = _('Compiling .mo file...')
+                print(color_text(f'📦 {message}', 'green'))
                 po_files = find_file(
-                    "messages.po",
+                    'messages.po',
                     LOCALES_DIR,
                     list_return=True,
                 )
                 mo_files = find_file(
-                    "messages.mo",
+                    'messages.mo',
                     LOCALES_DIR,
                     list_return=True,
                 )
                 for mo_file, po_file in zip(mo_files, po_files):
-                    print(color_text(f"📦 {mo_file}", "green"))
-                    run_command(f"msgfmt -o {mo_file} {po_file}")
+                    print(color_text(f'📦 {mo_file}', 'green'))
+                    run_command(f'msgfmt -o {mo_file} {po_file}')
 
                 # Removing the .pot file
-                run_command(f"rm {POT_FILE}")
+                run_command(f'rm {POT_FILE}')
 
                 # Remove files .po~
-                if find_file("messages.po~", LOCALES_DIR):
+                if find_file('messages.po~', LOCALES_DIR):
                     list_files: list = find_file(
-                        "messages.po~",
+                        'messages.po~',
                         LOCALES_DIR,
                         list_return=True,
                     )
                     for file in list_files:
-                        run_command(f"rm {file}")
+                        run_command(f'rm {file}')
 
-                message: str = _("Translations updated successfully!")
-                print(color_text(f"✅ {message}", "green"))
+                message: str = _('Translations updated successfully!')
+                print(color_text(f'✅ {message}', 'green'))
 
-            case "help":
+            case 'help':
                 print(
-                    "\n\nCommands:\n"
-                    "\n- python update_translations.py addlanguage [language]\n"  # noqa: E501
-                    "\tEx: python update_translations.py add_language pt_BR\n"  # noqa: E501
-                    "- python update_translations.py help: help\n"
-                    "- python update_translations.py update: update translations\n"  # noqa: E501
+                    '\n\nCommands:\n'
+                    '\n- python update_translations.py addlanguage [language]\n'  # noqa: E501
+                    '\tEx: python update_translations.py add_language pt_BR\n'  # noqa: E501
+                    '- python update_translations.py help: help\n'
+                    '- python update_translations.py update: update translations\n'  # noqa: E501
                 )
 
             case _:
-                message: str = _("Invalid command")
-                print(color_text(f"❌ {message}", "red"))
+                message: str = _('Invalid command')
+                print(color_text(f'❌ {message}', 'red'))
         return 0
     else:
-        message: str = _("No arguments provided")
-        print(color_text(f"❌ {message}", "red"))
+        message: str = _('No arguments provided')
+        print(color_text(f'❌ {message}', 'red'))
         return 1
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     update_translations()
